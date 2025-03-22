5bc5afa73a9d19fd83bbf8dbfd1a373b597d570c
Vaibhav <rushukla7@gmail.com>
2025-03-22 23:03:14 +0530

    added backend

--- server/src/middlewares/upload.middleware.js
+++ server/src/middlewares/upload.middleware.js
@@ -1,11 +1,14 @@
 import aws from "aws-sdk";
+import  dotenv  from "dotenv";
 import multer from "multer";
 import multerS3 from "multer-s3";
 import { v4 as uuidv4 } from "uuid";
 
+dotenv.config({ path: ".env" })
+
 aws.config.update({
-    secretAccessKey: "DvbTryK/3kKhpVtadr3eIKwcLldkUQI1KY29E7iW",
-    accessKeyId: "AKIAXYKJRBXVYAW2OYOF",
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
     region: 'us-east-1'
 })
 
